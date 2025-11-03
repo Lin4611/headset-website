47abb1529b260c9dbebb59c3ea93b5d7b06cf40a
Lin <tzu.hsun.lin046@gmail.com>
2025-11-03 12:19:49 +0800

    feat:new product section lg layout complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,11 @@ import logo from "./assets/img/icon/Logo.svg";
 import search_icon from "./assets/img/icon/Search.svg";
 import menu_icon from "./assets/img/icon/Justify-sm.svg";
 import sm_banner from "./assets/img/phone-pic/sm-banner.png";
-import banner from "./assets/img/lg-banner.jpg"
+import banner from "./assets/img/lg-banner.jpg";
+import bg_pic from "./assets/img/phone-pic/bg-sm.png";
+import p_1 from "./assets/img/phone-pic/new-sm-1.png";
+import p_2 from "./assets/img/phone-pic/new-sm-2.png"; 
+import p_3 from "./assets/img/phone-pic/new-sm-3.png";
 function App() {
   return (
     <>
@@ -77,8 +81,12 @@ function App() {
           </div>
         </section>
         <section className="hidden lg:flex w-full bg-dark h-[634px] items-center px-[312px] relative">
-          <img src={banner} alt="banner" className="w-[856px] h-[514px] object-cover" />
-           <div className="z-10 w-[636px] h-[139px] flex flex-col px-3 gap-4 absolute left-[972px]">
+          <img
+            src={banner}
+            alt="banner"
+            className="w-[856px] h-[514px] object-cover"
+          />
+          <div className="z-10 w-[636px] h-[139px] flex flex-col px-3 gap-4 absolute left-[972px]">
             <h2 className="text-h1 font-medium text-white">INEAR PRO MAX</h2>
             <div className="w-full flex flex-col justify-between">
               <p className="text-white font-normal">
@@ -88,6 +96,27 @@ function App() {
                 新色上市，搭載最新降噪技術，提供給您最高音質享受，採用適應性演算法，快速處理音效，讓您享受更高傳真的效果。
               </p>
             </div>
+          </div>
+        </section>
+        <section
+          className="w-full h-auto flex flex-col items-center bg-hero gap-12 py-15"
+          style={{ "--hero-desktop": `url(${bg_pic})` }}
+        > 
+          
+          <h2 className="text-white text-h2 font-medium">最新商品</h2>
+          <div className="flex flex-col w-full px-3">
+            <figure className="w-full flex flex-col gap-2">
+              <img src={p_1} alt="p1" className="w-full h-auto object-cover aspect-squares" />
+              <p className="w-full font-medium text-white p-2">INEAR PRO II</p>
+            </figure>
+            <figure className="w-full flex flex-col gap-2">
+              <img src={p_2} alt="p2" className="w-full h-auto object-cover aspect-squares" />
+              <p className="w-full font-medium text-white p-2">INEAR PRO MAX</p>
+            </figure>
+            <figure className="w-full flex flex-col gap-2">
+              <img src={p_3} alt="p3" className="w-full h-auto object-cover aspect-squares" />
+              <p className="w-full font-medium text-white p-2">INEAR PRO I</p>
+            </figure>
           </div>
         </section>
       </main>
